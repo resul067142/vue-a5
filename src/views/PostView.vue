b237d8f4d2a19461a3d992b141d85fd4e85e0060
resul067142 <resul067142@gmail.com>
2022-09-24 20:50:20 +0300

    proje ilk yükleme

---
+++ src/views/PostView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Başlık</td>
+                    <td>Açıklama</td>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr v-for="item in dataPostList" :key="item.id">
+                    <td>{{item.id}}</td>
+                    <td>{{item.title}}</td>
+                    <td>{{item.body}}</td>
+                </tr>
+
+
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            dataPostList: [],
+        }
+    },
+    created() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                this.dataPostList = response.data;
+            })
+    }
+}
+</script>
